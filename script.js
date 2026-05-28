1c807d59f8cf3d535ad354011afcc5623f2ae74a
ministerfaez-create <ministerfaez@gmail.com>
2026-05-28 17:38:21 +0200

    Implement product saving functionality
    Added a function to save product details to the database.

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log("LocallyMade Marketplace Running");
+window.saveProduct = async function(){
+
+  let productName =
+  document.getElementById("productName").value;
+
+  let productPrice =
+  document.getElementById("productPrice").value;
+
+  let productCategory =
+  document.getElementById("productCategory").value;
+
+  let productDescription =
+  document.getElementById("productDescription").value;
+
+  try{
+
+    await addDoc(collection(db,"products"),{
+
+      name: productName,
+      price: productPrice,
+      category: productCategory,
+      description: productDescription,
+      createdAt: new Date()
+
+    });
+
+    alert("Product Saved Successfully");
+
+  }
+
+  catch(error){
+
+    alert(error.message);
+
+  }
+
+}
